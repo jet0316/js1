faf0a68f308177ba04ad5c3d3f256312f389a3bf
Justin <jethielke@gmail.com>
2015-06-22 12:02:12 -0600

    js1+loop

--- main.js
+++ main.js
@@ -2,4 +2,46 @@
 
 var person = prompt("Please enter your name");
 
-alert('Hello ' + person)
+alert('Hello ' + person)	
+
+// for(var i = 0 ; i < 600; i += 100){
+//     console.log(i);
+// }
+
+
+// for(var i = 1; i < 128; i *= 2){
+//     console.log(i);
+// }
+
+// for(var i = 1; i < 4; i++ ){
+//     for(var d = 1; d < 4; d++){
+//     	console.log(i);
+//  	}
+//  }
+
+//  for(var i = 0 ; i < 12; i += 2){
+//     console.log(i);
+// }
+
+// for(var i = 3 ; i < 18; i += 3){
+//     console.log(i);
+// }
+
+// for(var i = 0; i < 4; i++){
+// 	for(var d =0; d < 4; d++){
+//    		 console.log(d);
+// 	}	
+// }
+
+
+
+
+
+// 0 100 200 300 400 500
+// 1 2 4 8 16 32 64
+// 1 1 1 2 2 2 3 3 3
+// 0 2 4 6 8 10
+// 3 6 9 12 15
+// 9 8 7 6 5 4 3 2 1 0
+// 0 1 2 3 0 1 2 3 0 1 2 3
+
